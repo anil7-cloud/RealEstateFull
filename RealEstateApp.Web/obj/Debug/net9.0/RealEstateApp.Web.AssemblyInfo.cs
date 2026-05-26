8932d82728e7aeb2b804ba050b6ac4d2ff077171
Adin Soyadin <mailin@example.com>
2026-05-26 22:20:29 +0300

    fix create agency interactive form

--- RealEstateApp.Web/obj/Debug/net9.0/RealEstateApp.Web.AssemblyInfo.cs
+++ RealEstateApp.Web/obj/Debug/net9.0/RealEstateApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442747edf11b4ae7daf6bc920063c3c6e9273f75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61cfed7e28ede797627ebe79e5ff41b1daacc6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
